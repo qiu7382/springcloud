a16ef76d9b34b79fe48189d8217f2e07dc104a87
DELL <qzh263491006@163.com>
2020-11-08 17:25:47 +0800

    springcloud配置中心,3355引用3344

---
+++ cloud2020/cloud-config-client-3355/src/main/java/com/qzh/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.qzh.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//自动刷新3355
+
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo+" " +serverPort;
+    }
+}
