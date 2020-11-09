a06b0680bb9e56475f290cd0bc731fe6f4874c3c
DELL <qzh263491006@163.com>
2020-11-09 20:34:06 +0800

    springcloud-stream,8801,8802,整合MQ实现消息发布订阅

---
+++ cloud2020/cloud-stream-rabbitmq-provider8801/src/main/java/com/qzh/springcloud/controller/SendMessageController.java
@@ -0,0 +1,18 @@
+package com.qzh.springcloud.controller;
+
+import com.qzh.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+}
