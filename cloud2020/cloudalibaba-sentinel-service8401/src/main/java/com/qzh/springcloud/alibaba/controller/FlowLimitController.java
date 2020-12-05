eefcc40bdee5bb30c40b6200d0ece0d3feee602a
qiu7382 <qzh263491006@163.com>
2020-12-05 16:22:20 +0800

    sentinel服务降级限流 1、流控，   QPS达到阈值限流，预热QPS/3并在时间内慢慢达到阈值，所有req进到队列排队等待一个个处理 2、降级   RT:QPS>=5&&响应时长>设定值降级。异常比例:QPS>=5&&异常比例>设定值降级。异常比例:QPS>=5&&异常数>设定值降级，单位分钟

--- cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/qzh/springcloud/alibaba/controller/FlowLimitController.java
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/qzh/springcloud/alibaba/controller/FlowLimitController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class FlowLimitController {
 
+    //流控Warm Up预热
+    //阈值/3默认值开始,设点时间内恢复
     @GetMapping("/testA")
     public String testA(){
         return "-----testA";
@@ -21,7 +23,26 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB(){
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
         return "-----testB";
     }
 
+    //降级策略RT当QPS>5&&响应时长超过阈值断路器打开
+    //异常比例,QPS>5&&异常比例达到百分比断路器打开
+    @GetMapping("/testD")
+    public String testD(){
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        int a = 10/0;
+
+        log.info("testD 测试异常比例"); return "------testD";
+    }
+
+    //异常数,分钟为单位大于异常数降级
+    @GetMapping("/testE")
+    public String testE(){
+        int a = 10/0;
+
+        log.info("test 测试异常数"); return "------testE";
+    }
 }
