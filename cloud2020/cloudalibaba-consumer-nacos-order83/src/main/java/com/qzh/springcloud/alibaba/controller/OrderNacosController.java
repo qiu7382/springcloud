2a48ccc3630b7cef4375086d170a1879d22d76bc
qiuzhenhua <qzh263491006@163.com>
2020-12-07 15:49:27 +0800

    nacos改造添加sentinel  83,9001,9002,controller层添加@sentinelResources,但未显示默认降级页面

--- cloud2020/cloudalibaba-consumer-nacos-order83/src/main/java/com/qzh/springcloud/alibaba/controller/OrderNacosController.java
+++ cloud2020/cloudalibaba-consumer-nacos-order83/src/main/java/com/qzh/springcloud/alibaba/controller/OrderNacosController.java
@@ -1,5 +1,8 @@
 package com.qzh.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.qzh.springcloud.entities.CommonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +22,11 @@ public class OrderNacosController {
     private String serverURL;
 
     @GetMapping(value = "/consumer/payment/nacos/{id}")
+    @SentinelResource(value = "paymentInfo", fallback = "handleException",blockHandler = "handleException")
     public String paymentInfo(@PathVariable("id") Integer id){
         return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+    public CommonResult handleException(BlockException exception){
+        return new CommonResult(444, exception.getClass().getCanonicalName() + " 服务不可用");
     }
 }
