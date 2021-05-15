17712bef923fbf6d0597246d857138ad9d40011d
qiu7382 <qzh263491006@163.com>
2021-05-15 17:21:30 +0800

    服务注册信息的显示配合discoveryClient使用，也要很多其他的功能，也可以注册其他注册中心

--- cloud2020/cloud-provider-payment8001/src/main/java/com/qzh/springcloud/PaymentMain8001.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/qzh/springcloud/PaymentMain8001.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//只能注册eureka的注册中心
 @EnableEurekaClient
+//服务注册信息的显示配合discoveryClient使用，也要很多其他的功能，也可以注册其他注册中心
 @EnableDiscoveryClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
